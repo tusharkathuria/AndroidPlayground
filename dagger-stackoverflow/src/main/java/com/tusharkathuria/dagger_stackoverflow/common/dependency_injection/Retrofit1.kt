c18de64a4419fb82e802590c489ca9c09869fcd9
Tushar Kathuria <tushar_kathuria@outlook.com>
2023-12-08 17:51:42 +0530

    Qualifiers

---
+++ dagger-stackoverflow/src/main/java/com/tusharkathuria/dagger_stackoverflow/common/dependency_injection/Retrofit1.kt
@@ -0,0 +1,6 @@
+package com.tusharkathuria.dagger_stackoverflow.common.dependency_injection
+
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class Retrofit1
--- dagger-stackoverflow/src/main/java/com/tusharkathuria/dagger_stackoverflow/common/dependency_injection/app/AppModule.kt
+++ dagger-stackoverflow/src/main/java/com/tusharkathuria/dagger_stackoverflow/common/dependency_injection/app/AppModule.kt
@@ -2,6 +2,7 @@ package com.tusharkathuria.dagger_stackoverflow.common.dependency_injection.app
 
 import android.app.Application
 import com.tusharkathuria.dagger_stackoverflow.Constants
+import com.tusharkathuria.dagger_stackoverflow.common.dependency_injection.Retrofit1
 import com.tusharkathuria.dagger_stackoverflow.networking.StackoverflowApi
 import dagger.Module
 import dagger.Provides
@@ -13,15 +14,24 @@ import javax.inject.Singleton
 class AppModule(private val application: Application) {
     @AppScope
     @Provides
-    fun retrofit() = Retrofit.Builder()
+    @Retrofit1  // We can  also use @Named("retrofit1") instead
+    fun retrofit1() = Retrofit.Builder()
         .baseUrl(Constants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+//    @AppScope
+//    @Provides
+//    @Retrofit2
+//    fun retrofit2() = Retrofit.Builder()
+//        .baseUrl("other_url")
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
 
     @Provides
     fun application(): Application = application
 
     @AppScope
     @Provides
-    fun stackOverflowApi(retrofit: Retrofit): StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    fun stackOverflowApi(@Retrofit1 retrofit: Retrofit): StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
 }
