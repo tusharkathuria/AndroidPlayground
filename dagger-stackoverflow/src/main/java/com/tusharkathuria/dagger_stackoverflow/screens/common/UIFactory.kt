dc786d70f13365dd3a4d7a32e033097695efeb5e
Tushar Kathuria <tushar_kathuria@outlook.com>
2023-12-08 20:01:17 +0530

    Providers

--- dagger-stackoverflow/src/main/java/com/tusharkathuria/dagger_stackoverflow/screens/common/UIFactory.kt
+++ dagger-stackoverflow/src/main/java/com/tusharkathuria/dagger_stackoverflow/screens/common/UIFactory.kt
@@ -2,13 +2,17 @@ package com.tusharkathuria.dagger_stackoverflow.screens.common
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.view.ViewParent
 import com.tusharkathuria.dagger_stackoverflow.common.image_loader.ImageLoader
 import com.tusharkathuria.dagger_stackoverflow.screens.questiondetails.QuestionDetailUI
 import com.tusharkathuria.dagger_stackoverflow.screens.questionlist.QuestionListUI
 import javax.inject.Inject
+import javax.inject.Provider
 
-class UIFactory @Inject constructor(private val layoutInflater: LayoutInflater, private val imageLoader: ImageLoader) {
+class UIFactory @Inject constructor(
+    private val layoutInflater: LayoutInflater,
+    private val imageLoaderProvider: Provider<ImageLoader>
+    // Using Provider ensures that object is provided while respecting conventions like scopes in the DI graph. This helps in late injection
+) {
     fun newQuestionListUI(parent: ViewGroup?): QuestionListUI = QuestionListUI(layoutInflater, parent)
-    fun newQuestionDetailUI(parent: ViewGroup?): QuestionDetailUI = QuestionDetailUI(layoutInflater, parent, imageLoader)
+    fun newQuestionDetailUI(parent: ViewGroup?): QuestionDetailUI = QuestionDetailUI(layoutInflater, parent, imageLoaderProvider.get())
 }
